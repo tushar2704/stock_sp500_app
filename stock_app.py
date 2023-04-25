36b14826d48debc135168bc0a1657545324f477c
tushar2704 <tushar.inseec@gmail.com>
2023-04-25 22:16:54 +0530

    app

--- stock_app.py
+++ stock_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import base64
 import matplotlib.pyplot as plt
-#import yfinance as yf
+import yfinance as yf
 import seaborn as sns
 # Application title and body
 st.set_page_config(page_title="Supermarket Sales Dashboard",
@@ -45,7 +45,7 @@ df_selected_sector = df[df['GICS Sector'].isin(selected_sector)]
 
 
 st.header("Display Compaines in Selected Sector")
-st.write("Data Dimension :"+str(df_selected_sector.shape[0])+'rows and '+str(df_selected_sector[1])+' columns.')
+st.write('Data Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' + str(df_selected_sector.shape[1]) + ' columns.')
 st.dataframe(df_selected_sector)
 
 # Download S&P 500 data
@@ -56,6 +56,37 @@ def filedownload(df):
     href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
     return href
 
+st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
+
+data = yf.download(
+    tickers= list(df_selected_sector[:10].Symbol),
+    period='ytd',
+    interval='1d',
+    group_by='ticker',
+    auto_adjust=True,
+    prepost=True,
+    threads=True,
+    proxy=None
+)
+
+# Plotting Closing Price of Query Symbol
+def price_plot(symbol):
+    df= pd.DataFrame(data[symbol].Close)
+    df['Date'] = df.index
+    plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
+    plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+    plt.xticks(rotation=90)
+    plt.title(symbol, fontweight='bold')
+    plt.xlabel("Date", fontweight='bold')
+    plt.ylabel("Closing Price", fontweight='bold')
+    return st.pyplot()
+
+num_company = st.sidebar.slider("Number of Companies", 1,5)
+
+if st.button("Show Plots"):
+    st.header("Stock Closing Price")
+    for i in list(df_selected_sector.Symbol)[:num_company]:
+        price_plot(i)
 
 
 
