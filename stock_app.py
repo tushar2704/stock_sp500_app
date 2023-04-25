91d456b51184b8cabb38309ebbcb2e373e5f349d
tushar2704 <tushar.inseec@gmail.com>
2023-04-25 21:59:54 +0530

    app

--- stock_app.py
+++ stock_app.py
@@ -25,4 +25,46 @@ st.sidebar.header("Please choose: ")
 
 # Webscraping of S&P500 data
 
-#@st.cache_data(persist=True)
+@st.cache_data(persist=True)
+def load_data():
+    url='https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    html =pd.read_html(url, header=0)
+    df=html[0]
+    return df
+
+df = load_data()
+sector = df.groupby('GICS Sector')
+
+# Sidebar for Sector selection
+
+sorted_secotr_unique = sorted(df['GICS Sector'].unique())
+selected_sector = st.sidebar.multiselect("Sector", sorted_secotr_unique, sorted_secotr_unique)
+
+# Filtering data
+df_selected_sector = df[df['GICS Sector'].isin(selected_sector)]
+
+
+st.header("Display Compaines in Selected Sector")
+st.write("Data Dimension :"+str(df_selected_sector.shape[0])+'rows and '+str(df_selected_sector[1])+' columns.')
+st.dataframe(df_selected_sector)
+
+# Download S&P 500 data
+
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+    return href
+
+
+
+
+
+
+
+
+
+
+
+
+
